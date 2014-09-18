0b805405f676b5f9f6c3a2743f7de6c6b84f97fd
Michel Megens <dev@michelmegens.net>
2014-09-18 21:07:53 +0200

    avr: added bitops and type definitions.
    Added definitions for bit ops and types for the 8- and 16-bit AVR
    architectures.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ include/etaos/types.h
@@ -0,0 +1,29 @@
+/*
+ *  Eta/OS - ETA/OS types
+ *  Copyright (C) 2014   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ETAOS_TYPES_H_
+#define __ETAOS_TYPES_H_
+
+typedef unsigned char bool;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+
+typedef char int8_t;
+typedef short int16_t;
+
+#endif
