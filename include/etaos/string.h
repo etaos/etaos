43a5bf1f71c00ee2249b382230214f8d3e0f4acb
Michel Megens <dev@michelmegens.net>
2014-09-29 16:00:47 +0200

    libc: string: added a string maniplation module.
    lib/string contains the '<string.h>' part of libc. These functions are
    all, in some way, relevant to C style strings (char *). The functions
    implemented in v1 are:
    
    	* strcmp (compare 2 strings)
    	* strchr (find first occurance of a character)
    	* strlen (find the length of a string)
    	* strnlen (see if a string is longer than a given amount)
    
    The return value of strnlen is the value of strlen if the string is NOT
    longer than the given amount, if it is the given length is returned.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ include/etaos/string.h
@@ -0,0 +1,29 @@
+/*
+ *  ETA/OS - LibC <string.h>
+ *  Copyright (C) 2014   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __STRING_H__
+#define __STRING_H__
+
+#include <etaos/types.h>
+
+extern char *strchr(const char *str, int c);
+extern int strlen(const char *str);
+extern int strnlen(const char *str, size_t size);
+extern int strcmp(const char *s1, const char *s2);
+
+#endif
---
+++ lib/string/strcmp.c
@@ -0,0 +1,30 @@
+/*
+ *  ETA/OS - String Compare
+ *  Copyright (C) 2014   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <etaos/kernel.h>
+#include <etaos/string.h>
+
+int strcmp(const char *s1, const char *s2)
+{
+	while(*s1 == *s2++) {
+		if(*s1++ == 0)
+			return 0;
+	}
+
+	return (*(unsigned char *) s1 - *(unsigned char *) --s2);
+}
