74e585bb8d15381817db4cd6c326ab95f339bf4a
Michel Megens <dev@michelmegens.net>
2014-09-30 23:07:38 +0200

    usart: core: implemented the core usart structure.
    Implemented the core data structure for the USART device driver. The
    USART device driver is located at drivers/usart/usart-core.c. A platform
    specific driver is then using that driver to implement the hardware in
    drivers/usart/usart-<platform-name>.c
    
    The USART data structure contains:
    
    	* a lock (mutex)
    	* device structure (embedded, no pointer)
    	* read fpointer
    	* write fpointer
    	* timeout for reads and writes
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ include/etaos/usart.h
@@ -0,0 +1,38 @@
+/*
+ *  ETA/OS - USART driver header
+ *  Copyright (C) 2014   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __USART_H__
+#define __USART_H__
+
+#include <etaos/kernel.h>
+#include <etaos/types.h>
+#include <etaos/device.h>
+#include <etaos/mutex.h>
+
+struct usart {
+	mutex_t bus_lock;
+	int timeout;
+
+	struct device dev;
+	int (*write)(struct usart *uart, const void *tx,
+			size_t txlen, unsigned int baud);
+	int (*read)(struct usart *uart, void *rx, size_t rxlen,
+			unsigned int baud);
+};
+
+#endif
