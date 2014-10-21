c16db9458f69236e435d8425aa00eb4d5183e9d7
Michel Megens <dev@michelmegens.net>
2014-10-21 19:00:13 +0200

    gpio: doc: documented all GPIO API functions
    Documented these functions:
    
    	* gpio_chip_init
    	* gpio_pin_init
    	* gpio_chip_to_pin
    	* gpio_pin_request
    	* gpio_direction_input
    	* gpio_direction_output
    	* raw_gpio_direction_ouput
    	* raw_gpio_pin_write
    	* gpio_pin_write
    	* gpio_pin_read
    	* gpio_get_direction
    	* raw_gpio_read_pin
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -26,6 +26,12 @@
 
 struct gpio_chip *gpio_sys_chip;
 
+/**
+ * @brief Initialise a new GPIO chip.
+ * @param chip Chip which has to be initialised
+ * @param pins Amount of pins that belong to \p chip.
+ * @return An error code, -EOK on success.
+ */
 int gpio_chip_init(struct gpio_chip *chip, uint16_t pins)
 {
 	int err;
@@ -42,6 +48,13 @@ int gpio_chip_init(struct gpio_chip *chip, uint16_t pins)
 	return err;
 }
 
+/**
+ * @brief Initialise a new pin descriptor.
+ * @param chip The chip where the pin belongs to.
+ * @param pin The pin that has to be initialised.
+ * @param nr GPIO pin number.
+ * @param flags GPIO pin flags.
+ */
 void gpio_pin_init(struct gpio_chip *chip, 
 		struct gpio_pin *pin, uint16_t nr, unsigned long flags)
 {
@@ -54,6 +67,12 @@ void gpio_pin_init(struct gpio_chip *chip,
 	pin->nr = nr;
 }
 
+/**
+ * @brief Get a pin descriptor based on its pin number.
+ * @param chip GPIO chip where the pin belongs to.
+ * @param nr Pin number.
+ * @retrun The GPIO pin descriptor.
+ */
 struct gpio_pin *gpio_chip_to_pin(struct gpio_chip *chip, uint16_t nr)
 {
 	if(!chip)
@@ -62,6 +81,12 @@ struct gpio_pin *gpio_chip_to_pin(struct gpio_chip *chip, uint16_t nr)
 	return &chip->pins[nr];
 }
 
+/**
+ * @brief Request a pin for usage.
+ * @param pin Pin to request.
+ * @return Error code, zero on success.
+ * @warning ALL pins have to be requested before usage.
+ */
 int gpio_pin_request(struct gpio_pin *pin)
 {
 	struct gpio_chip *chip;
@@ -81,6 +106,11 @@ int gpio_pin_request(struct gpio_pin *pin)
 
 #define pin_is_requested(__p) test_bit(GPIO_REQUESTED, &(__p)->flags)
 
+/**
+ * @brief Configure a pin as input.
+ * @param pin Pin to configure as input.
+ * @return An error code.
+ */
 int gpio_direction_input(struct gpio_pin *pin)
 {
 	struct gpio_chip *chip;
@@ -102,6 +132,12 @@ int gpio_direction_input(struct gpio_pin *pin)
 	return err;
 }
 
+/**
+ * @brief Set a pin to OUTPUT.
+ * @param pin Pin to configure as output.
+ * @param value Value to write to the pin.
+ * @return Error code. Zero on success.
+ */
 int gpio_direction_output(struct gpio_pin *pin, int value)
 {
 	if(test_bit(GPIO_ACTIVE_LOW, &pin->flags))
@@ -110,6 +146,12 @@ int gpio_direction_output(struct gpio_pin *pin, int value)
 	return raw_gpio_direction_ouput(pin, value);
 }
 
+/**
+ * @brief Set a pin to OUTPUT.
+ * @param pin Pin to configure as output.
+ * @param value Value to write to the pin.
+ * @return Error code. Zero on success.
+ */
 int raw_gpio_direction_ouput(struct gpio_pin *pin, int value)
 {
 	struct gpio_chip *chip;
@@ -137,6 +179,13 @@ int raw_gpio_direction_ouput(struct gpio_pin *pin, int value)
 	return err;
 }
 
+/**
+ * @brief Write a boolean value to a GPIO pin.
+ * @param pin Pin to write to.
+ * @param val Value to write.
+ * @return Error code. Zero on success.
+ * @note This function doesn't take GPIO_ACTIVE_LOW into account.
+ */
 int raw_gpio_pin_write(struct gpio_pin *pin, int val)
 {
 	struct gpio_chip *chip;
@@ -156,6 +205,12 @@ int raw_gpio_pin_write(struct gpio_pin *pin, int val)
 		return -EINVAL;
 }
 
+/**
+ * @brief Write a boolean value to a GPIO pin.
+ * @param pin Pin to write to.
+ * @param val Value to write.
+ * @return Error code. Zero on success.
+ */
 int gpio_pin_write(struct gpio_pin *pin, int val)
 {
 	if(test_bit(GPIO_ACTIVE_LOW, &pin->flags))
@@ -170,7 +225,11 @@ int gpio_pin_write(struct gpio_pin *pin, int val)
 	return raw_gpio_pin_write(pin, val);
 }
 
-
+/**
+ * @brief Read the value of a gpio pin.
+ * @param pin Pin to read.
+ * @return Value of \p pin.
+ */
 int gpio_pin_read(struct gpio_pin *pin)
 {
 	int val;
@@ -185,6 +244,13 @@ int gpio_pin_read(struct gpio_pin *pin)
 	return val;
 }
 
+/**
+ * @brief Get the direction of a GPIO pin.
+ * @param pin Pin to get the direction of.
+ * @return The direction.
+ * @retval GPIO_DIR_IN if the direction is input.
+ * @retval GPIO_DIR_OUT if the direction is output.
+ */
 int gpio_get_direction(struct gpio_pin *pin)
 {
 	int status = -EINVAL;
@@ -206,6 +272,11 @@ int gpio_get_direction(struct gpio_pin *pin)
 	return status;
 }
 
+/**
+ * @brief Read the value of a gpio pin.
+ * @param pin Pin to read.
+ * @return Value of \p pin.
+ */
 int raw_gpio_read_pin(struct gpio_pin *pin)
 {
 	struct gpio_chip *chp;
