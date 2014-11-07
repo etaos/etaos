038df919ba43f6174f6fc4a7ff22b8e61c79e245
Michel Megens <dev@michelmegens.net>
2014-11-07 07:10:32 +0100

    kbuild: single obj mod building fix
    Single object modules were causing trouble if more then one of them were
    being build in the same directory due to using a bogus variable. Fixed
    by using a derative of the make $@ variable.
    
    Added a module for the generic EEPROM driver.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ drivers/eeprom/eeprom.c
@@ -0,0 +1,31 @@
+/*
+ *  ETA/OS - EEPROM header
+ *  Copyright (C) 2014   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <etaos/kernel.h>
+#include <etaos/types.h>
+#include <etaos/i2c.h>
+#include <etaos/eeprom.h>
+#include <etaos/mem.h>
+#include <etaos/bitops.h>
+#include <etaos/init.h>
+
+static void __used eeprom_init(void)
+{
+}
+
+subsys_init(eeprom_init);
