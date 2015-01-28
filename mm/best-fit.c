025c649dc4782b37bef34e8cde7d92cc63daf935
Michel Megens <dev@michelmegens.net>
2015-01-28 17:02:29 +0100

    mm: bf: implemented the best fit allocation.
    Implemented an initial version for the best fit allocator. Please note
    that the allocator is not functional as of now, the 'free' mechanism is
    still not functional.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- mm/best-fit.c
+++ mm/best-fit.c
@@ -43,8 +43,62 @@ void mm_init(void *start, size_t size)
 	mm_init_node(start, size - sizeof(*mm_head));
 }
 
+static inline bool bf_select_new_node(struct heap_node *old,
+		struct heap_node *new)
+{
+	if(old) {
+		if(old->size > new->size)
+			return true;
+	}
+
+	return false;
+}
+
 MEM void *mm_alloc(size_t size)
 {
+	void *rv;
+	struct heap_node *c, *bf, *prev;
+
+	spin_lock(&mlock);
+	c = mm_head;
+	prev = NULL;
+	bf = NULL;
+
+	while(c) {
+		if(c->size == size)
+			break;
+
+		if(c->size > size) {
+			if(c->size <= size+sizeof(*c)+4)
+				break;
+
+			if(bf_select_new_node(bf, c)) {
+				bf = c;
+				rv = prev;
+			}
+		}
+
+		prev = c;
+		c = c->next;
+	}
+
+	if(c == NULL && bf == NULL)
+		goto err_l;
+
+	if(c == NULL && bf != NULL) {
+		if(bf->size > size+sizeof(*bf)+4)
+			mm_split_node(bf, size);
+		c = bf;
+		prev = rv;
+	}
+
+	mm_use_block(c, prev);
+	rv = c;
+	rv += sizeof(*c);
+	return rv;
+
+err_l:
+	spin_unlock(&mlock);
 	return NULL;
 }
 
