9fa31c0e2876dfef52d6258650530f896075ef93
Michel Megens <dev@michelmegens.net>
2015-02-02 14:46:49 +0100

    spi: core: setup of the SPI subsystem
    This patch contains the setup for the SPI subsystem. The driver isn't
    functional yet, only the build files are added.
    
    Source and header files have some basic driver include directives added.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ drivers/spi/spidev.c
@@ -0,0 +1,26 @@
+/*
+ *  ETA/OS - SPI driver
+ *  Copyright (C) 2015   Michel Megens <dev@michelmegens.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <etaos/kernel.h>
+#include <etaos/error.h>
+#include <etaos/types.h>
+#include <etaos/spi.h>
+#include <etaos/bitops.h>
+#include <etaos/init.h>
+#include <etaos/time.h>
+#include <etaos/tick.h>
