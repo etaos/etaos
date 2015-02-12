f5da549c51703c07e6bc5537a36e2de059d6bd08
Michel Megens <dev@michelmegens.net>
2015-02-12 12:26:02 +0100

    sched: test: simplified the scheduling test
    Simplified the scheduling test. GPIO operations don't belong in the
    scheduling unit test. Removed all of them.
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

--- usr/tests/schedtest/sched.c
+++ usr/tests/schedtest/sched.c
@@ -5,6 +5,7 @@
 
 #include <etaos/kernel.h>
 #include <etaos/stdio.h>
+#include <etaos/string.h>
 #include <etaos/mem.h>
 #include <etaos/sched.h>
 #include <etaos/thread.h>
@@ -13,8 +14,6 @@
 #include <etaos/mutex.h>
 #include <etaos/time.h>
 #include <etaos/tick.h>
-#include <etaos/gpio.h>
-#include <etaos/platform.h>
 #include <etaos/ipm.h>
 
 static unsigned char test_thread_stack[CONFIG_STACK_SIZE];
@@ -41,7 +40,6 @@ THREAD(test_th_handle, arg)
 int main(void)
 {
 	const char * ip_msg = "IPM message\n";
-	bool value = true;
 	int64_t tick_orig;
 
 	printf("Application started (M:%u)!\n", mm_heap_available());
@@ -49,25 +47,17 @@ int main(void)
 	test_t = thread_create( "tst", &test_th_handle, NULL,
 			CONFIG_STACK_SIZE, test_thread_stack, 80);
 
-	pgpio_pin_request(13);
-	pgpio_direction_output(13, false);
-	pgpio_pin_release(13);
-
 	tick_orig = sys_tick;
-
+	
 	while(true) {
 		if(time_after(sys_tick, tick_orig + 3000))
 			printf("tm after: 3000\n");
 		ipm_post_msg(&ipm_q, ip_msg, strlen(ip_msg));
 		printf("maint mem: %u\n", mm_heap_available());
 
-		pgpio_pin_request(13);
-		pgpio_write_pin(13, value);
-		pgpio_pin_release(13);
-		value = !value;
-
 		sleep(500);
 	}
+
 	return 0;
 }
 
