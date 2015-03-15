00a2032fdb6a106b70a75407092041fa15f1c637
Michel Megens <dev@michelmegens.net>
2015-03-15 16:39:19 +0100

    fs: romfs: added a romfs header
    Added a header file for the read-only flash file system. The
    structure romfs describes the romfs file system entries (on a
    filesystem specific level).
    
    Signed-off-by: Michel Megens <dev@michelmegens.net>

---
+++ include/etaos/romfs.h
@@ -0,0 +1,34 @@
+/*
+ *  ETA/OS - ROMFS header
+ *  Copyright (C)  2015   Michel Megens
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ROMFS_H__
+#define __ROMFS_H__
+
+#include <etaos/kernel.h>
+#include <etaos/types.h>
+
+#include <asm/pgm.h>
+
+struct romfs {
+	struct romfs *next;
+	const char *name;
+	size_t length;
+	const char *data;
+};
+
+#endif
