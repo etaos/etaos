c77cc0a7ad7ff6fb3cb5734e17af0de797723fe4
Michel Megens <dev@bietje.net>
2016-09-06 06:18:48 +0200

    doc: power: added docs to the power management module
    Signed-off-by: Michel Megens <dev@bietje.net>

--- include/etaos/power.h
+++ include/etaos/power.h
@@ -16,28 +16,62 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file include/etaos/power.h Power management
+ */
+
 #ifndef __POWER_H__
 #define __POWER_H__
 
+/**
+ * @addtogroup power
+ * @{
+ */
+
 #include <etaos/kernel.h>
 
-#define POWER_IDLE	0x1
-#define POWER_STANDBY	0x2
-#define POWER_SAVE	0x4
-#define POWER_DOWN	0x8
+/**
+ * @name Power saving modes
+ * @{
+ */
+#define POWER_IDLE	0x1 //!< Idle power mode.
+#define POWER_STANDBY	0x2 //!< Standby power mode.
+#define POWER_SAVE	0x4 //!< Power save mode.
+#define POWER_DOWN	0x8 //!< Power down mode.
+/** @} */
 
+/**
+ * @ingroup archAPI
+ * @brief Hibernate the CPU into the selected power mode.
+ * @note This function is blocking, based on the selected mode.
+ */
 extern void arch_hibernate(void);
+/**
+ * @ingroup archAPI
+ * @brief Select a power mode to hibernate into.
+ * @param mode Power saving mode to select.
+ */
 extern void arch_set_power_mode(int mode);
 
+/**
+ * @ingroup archAPI
+ * @brief Select a power mode to hibernate into.
+ * @param mode Power saving mode to select.
+ */
 static inline void power_set_mode(int mode)
 {
 	arch_set_power_mode(mode);
 }
 
+/**
+ * @brief Hibernate the CPU into the selected power mode.
+ * @note This function is blocking, based on the selected mode.
+ */
 static inline void hibernate(void)
 {
 	arch_hibernate();
 }
 
+/** @} */
 #endif /* __POWER_H__ */
 
