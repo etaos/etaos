6af0759c44b9e8ce16d150fc84d7313c09d4b3bb
Michel Megens <dev@bietje.net>
2016-10-05 01:12:16 +0200

    tests: automate the preemption and scheduling test
    Automate the preemption and scheduling tests by integrating them into
    Calypso.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- usr/tests/preempt/sched.c
+++ usr/tests/preempt/sched.c
@@ -5,13 +5,19 @@
 
 #include <etaos/kernel.h>
 #include <etaos/stdio.h>
+#include <etaos/error.h>
 #include <etaos/delay.h>
 #include <etaos/thread.h>
 #include <etaos/sched.h>
 #include <etaos/mem.h>
 
+#include <uapi/etaos/test.h>
+
+#include <asm/pgm.h>
+
 static unsigned char test_thread_stack[CONFIG_STACK_SIZE];
 static struct thread *test_t;
+static volatile int counter;
 
 THREAD(test_th_handle, arg)
 {
@@ -20,24 +26,38 @@ THREAD(test_th_handle, arg)
 	while(true) {
 		if(*trigger) {
 			*trigger = false;
+			if(counter++ == 5)
+				break;
 		}
 	}
+
+	kill();
 }
 
 int main(void)
 {
-	int trigger = true;
+	int trigger = true, i = 0;
 
-	printf("Application started (M:%u)!\n", mm_heap_available());
+	printf_P(PSTR("Application started!\n"));
+
+	counter = 0;
 	test_t = thread_create( "tst", &test_th_handle, (void*)&trigger,
 			CONFIG_STACK_SIZE, test_thread_stack, 120);
 
-	while(true) {
+	for(; i < 5; i++) {
 		trigger = true;
-		printf("main_thread (%u)\n", mm_heap_available());
+		printf_P(PSTR("Main thread!\n"));
 		sleep(500);
 	}
 
-	return 0;
+	printf_P(PSTR("Preempt test: "));
+	if(counter == 5)
+		printf_P(PSTR("[OK]"));
+	else
+		printf_P(PSTR("[ERROR]"));
+	putc('\n', stdout);
+
+	printf(CALYPSO_EXIT);
+	return -EOK;
 }
 
--- usr/tests/sched/sched.c
+++ usr/tests/sched/sched.c
@@ -5,16 +5,17 @@
 
 #include <etaos/kernel.h>
 #include <etaos/stdio.h>
+#include <etaos/error.h>
 #include <etaos/string.h>
 #include <etaos/mem.h>
-#include <etaos/sched.h>
 #include <etaos/thread.h>
-#include <etaos/evm.h>
 #include <etaos/mem.h>
-#include <etaos/mutex.h>
 #include <etaos/time.h>
 #include <etaos/tick.h>
 #include <etaos/ipm.h>
+
+#include <asm/pgm.h>
+#include <uapi/etaos/test.h>
 
 static unsigned char test_thread_stack[CONFIG_STACK_SIZE];
 static struct thread *test_t;
@@ -24,41 +25,46 @@ THREAD(test_th_handle, arg)
 {
 	int fd;
 	struct ipm msg;
+	volatile int *idx = arg;
 
 	fd = open("atm-usart", _FDEV_SETUP_RW);
 
 	nice(150);
 
-	while(true) {
+	while(*idx < 2) {
 		ipm_get_msg(&ipm_q, &msg);
 		ipm_reset_queue(&ipm_q);
 		printf("[test]:\t");
 		write(fd, msg.data, msg.len);
 		printf("[test]:\ttest_thread\n");
 	}
+
+	kill();
 }
 
 int main(void)
 {
 	const char * ip_msg = "IPM message\n";
 	int64_t tick_orig;
+	int idx = 0;
 
-	printf("Application started (M:%u)!\n", mm_heap_available());
+	printf_P(PSTR("Application started!\n"));
 	ipm_queue_init(&ipm_q, 2);
-	test_t = thread_create( "tst", &test_th_handle, NULL,
-			CONFIG_STACK_SIZE, test_thread_stack, 80);
+	test_t = thread_create( "tst", &test_th_handle, &idx,
+			CONFIG_STACK_SIZE, test_thread_stack, 130);
 
 	tick_orig = sys_tick;
-	
-	while(true) {
-		if(time_after(sys_tick, tick_orig + 3000))
-			printf("[main]:\ttm after: 3000\n");
+
+	for(; idx < 5; idx++) {
+		if(time_after(sys_tick, tick_orig + 1000))
+			printf("[main]:\ttm after: 1000\n");
 		ipm_post_msg(&ipm_q, ip_msg, strlen(ip_msg));
-		printf("[main]:\tmem: %u\n", mm_heap_available());
+		printf("[main]:\tIPM posted\n");
 
 		sleep(500);
 	}
 
-	return 0;
+	printf(CALYPSO_EXIT);
+	return -EOK;
 }
 
