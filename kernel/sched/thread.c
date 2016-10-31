4a9a9cfbca0b03b3f37baea999056efbdfc6d256
Michel Megens <dev@bietje.net>
2016-10-31 04:54:25 +0100

    Revert "sched: core: add default scheduling priority's"
    This patch breaks the core build. It messes up the C++ definitions,
    probably somewhere in <etaos/thread.h>.
    
    This reverts commit e0054db807e93f3d34b33649db58edd199e6496f.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- kernel/sched/thread.c
+++ kernel/sched/thread.c
@@ -83,8 +83,7 @@ void sched_init_idle(struct thread *tp, const char *name,
 		thread_handle_t handle, void *arg, size_t stack_size, 
 		void *stack)
 {
-	raw_thread_init(tp, name, handle, arg, stack_size,
-			stack, SCHED_LOWEST_PRIO);
+	raw_thread_init(tp, name, handle, arg, stack_size, stack, 255);
 	set_bit(THREAD_IDLE_FLAG, &tp->flags);
 }
 
