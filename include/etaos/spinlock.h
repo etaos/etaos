20c2516b07ff90c255e579f1fb7c1b4ed5b48370
Michel Megens <dev@bietje.net>
2016-10-31 10:58:08 +0100

    kernel: doc: document the spinlock API
    Add (doxygen) documentation to the spinlock header files.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- include/etaos/spinlock.h
+++ include/etaos/spinlock.h
@@ -16,6 +16,15 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @file etaos/spinlock.h Spin lock header
+ */
+
+/**
+ * @addtogroup kernel
+ * @{
+ */
+
 #ifndef __SPINLOCK_H__
 #define __SPINLOCK_H__
 
@@ -62,18 +71,36 @@ static inline void spin_unlock(spinlock_t *lock)
 #endif
 }
 
+/**
+ * @brief Lock a spin lock.
+ * @param lock Spin lock to lock.
+ * @note This function has the same behaviour as #spin_lock, but it
+ *       is defined to provide naming compatibility with #_raw_spin_unlock.
+ */
 static inline void _raw_spin_lock(spinlock_t *lock)
 {
 	preempt_disable();
 	raw_spin_lock(lock);
 }
 
+/**
+ * @brief Unlock a spin lock.
+ * @param lock Spin lock to unlock..
+ *
+ * After the lock has been released, preemption will be reenabled, but
+ * an immediate resched will be avoided by using preempt_enable_no_resched.
+ */
 static inline void _raw_spin_unlock(spinlock_t *lock)
 {
 	raw_spin_unlock(lock);
 	preempt_enable_no_resched();
 }
 
+/**
+ * @brief Lock a spin lock and disable the interrupts.
+ * @param lock Lock which needs to be locked.
+ * @param flags Variable to store the interrupt flags in.
+ */
 static inline void _spin_lock_irqsave(spinlock_t *lock, unsigned long *flags)
 {
 	preempt_disable();
@@ -81,6 +108,11 @@ static inline void _spin_lock_irqsave(spinlock_t *lock, unsigned long *flags)
 	raw_spin_lock(lock);
 }
 
+/**
+ * @brief Unlock a spin lock and restore the interrupt flags.
+ * @param lock Lock which needs to be unlocked.
+ * @param flags The interrupt flags which need to be restored.
+ */
 static inline void _spin_unlock_irqrestore(spinlock_t *lock, 
 					   unsigned long *flags)
 {
@@ -89,6 +121,11 @@ static inline void _spin_unlock_irqrestore(spinlock_t *lock,
 	preempt_enable();
 }
 
+/**
+ * @brief Lock a spin lock and disable the interrupts.
+ * @param lock Lock which needs to be locked.
+ * @param flags Variable to store the interrupt flags in.
+ */
 static inline void _raw_spin_lock_irqsave(spinlock_t *lock, unsigned long *flags)
 {
 	preempt_disable();
@@ -96,6 +133,11 @@ static inline void _raw_spin_lock_irqsave(spinlock_t *lock, unsigned long *flags
 	raw_spin_lock(lock);
 }
 
+/**
+ * @brief Unlock a spin lock and restore the interrupt flags.
+ * @param lock Lock which needs to be unlocked.
+ * @param flags The interrupt flags which need to be restored.
+ */
 static inline void _raw_spin_unlock_irqrestore(spinlock_t *lock, 
 					   unsigned long *flags)
 {
@@ -107,6 +149,7 @@ static inline void _raw_spin_unlock_irqrestore(spinlock_t *lock,
 /**
  * @brief Lock a spin lock without touching the preemption settings.
  * @param lock Spin lock that has to be locked.
+ * @param flags Variable to save the interrupt flags.
  * @note The local interrupts will also be disabled.
  */
 static inline void raw_spin_lock_irq(spinlock_t *lock, unsigned long *flags)
@@ -118,6 +161,7 @@ static inline void raw_spin_lock_irq(spinlock_t *lock, unsigned long *flags)
 /**
  * @brief Unlock a spin lock without touching the preemption settings.
  * @param lock Spin lock that has to be unlocked.
+ * @param flags Variable containing the IRQ's that need to be restored.
  * @note The local interrupts will also be enabled.
  */
 static inline void raw_spin_unlock_irq(spinlock_t *lock, unsigned long *flags)
@@ -145,7 +189,18 @@ static inline int spin_try_lock(spinlock_t *lock)
 
 CDECL_END
 
+/**
+ * @brief Lock a spin lock and disable the interrupts.
+ * @param _l Lock which needs to be locked.
+ * @param _f Variable to store the interrupt flags in.
+ */
 #define spin_lock_irqsave(_l, _f) _spin_lock_irqsave(_l, &_f)
+
+/**
+ * @brief Unlock a spin lock and restore the interrupt flags.
+ * @param _l Lock which needs to be unlocked.
+ * @param _f The interrupt flags which need to be restored.
+ */
 #define spin_unlock_irqrestore(_l, _f) _spin_unlock_irqrestore(_l, &_f)
 
 #define raw_spin_lock_irqsave(__l, __f) _raw_spin_lock_irqsave(__l, &__f)
@@ -154,4 +209,6 @@ CDECL_END
 
 
 #endif /* __SPINLOCK_H__ */
+
+/** @} */
 
