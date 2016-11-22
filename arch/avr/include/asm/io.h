456b4797f38fe746ea2a246fa1f530c71c667f61
Michel Megens <dev@bietje.net>
2016-11-22 15:38:33 +0100

    sched: avr: remove the INIT_STACK_SIZE Kconfig option
    The init stack is no longer used (main stack is directly initialised).
    Therefore there is no need for the INIT_STACK_SIZE Kconfig option.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- arch/avr/include/asm/io.h
+++ arch/avr/include/asm/io.h
@@ -47,8 +47,6 @@
 #include <asm/iom1280.h>
 #endif
 
-#define INIT_STACK_SIZE CONFIG_INIT_STACK_SIZE
-
 #define irq_enter_critical() __asm__ __volatile__( \
 		"in __tmp_reg__, __SREG__" "\n\t" \
 		"cli"			   "\n\t" \
