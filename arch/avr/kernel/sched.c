8040849bbf58b2c0691985cbb10ddd5c904108b7
Michel Megens <dev@bietje.net>
2016-12-03 17:44:48 +0100

    avr: kernel: only use RAMPD if present
    Check if the RAMPD address is present on the core before attempting to
    use it in the AVR scheduling core.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- arch/avr/kernel/sched.c
+++ arch/avr/kernel/sched.c
@@ -87,7 +87,11 @@ struct avr_stack_frame {
 	uint8_t r31, r30,
 		r29, r28, r27, r26, r25, r24, r23, r22, r21, r20,
 		r19, r18, r17, r16, r15, r14, r13, r12, r11, r10,
-		r9, r8, r7, r6, r5, r4, r3, r2, r1, status, rampd, r0;
+		r9, r8, r7, r6, r5, r4, r3, r2, r1, status;
+#ifdef AVR_RAMPD_ADDR
+	uint8_t rampd;
+#endif
+	uint8_t r0;
 
 #ifdef AVR_22BIT_PC
 	uint8_t handle_ext;
