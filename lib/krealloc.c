1c3dd691c2dcd7fae0662a438a454f754ad55df8
Michel Megens <dev@bietje.net>
2016-12-05 03:28:48 +0100

    lib: doc: add krealloc to the libc group
    Add krealloc the the libc Doxygen documentation module.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- lib/krealloc.c
+++ lib/krealloc.c
@@ -16,6 +16,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup libc
+ * @{
+ */
+
 #include <etaos/kernel.h>
 #include <etaos/types.h>
 #include <etaos/mem.h>
@@ -57,4 +62,6 @@ void *krealloc(void *old, size_t newsize)
 	kfree(old);
 	return new;
 }
+
+/** @} */
 
