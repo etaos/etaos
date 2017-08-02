85a3f85f185351abebfc699b68834e23663e8f2b
Michel Megens <dev@bietje.net>
2017-08-02 22:24:38 +0200

    kernel: assert: implement assert macro
    Implement an assert macro to assert the value's of variables before
    continueing with the execution of a function or method.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

---
+++ include/etaos/assert.h
@@ -0,0 +1,27 @@
+/*
+ *  ETA/OS - GPIO driver
+ *  Copyright (C) 2017   Michel Megens <dev@bietje.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ASSERT_H__
+#define __ASSERT_H__
+
+#define ASSERT(__condi__) \
+	(__condi) ? (void)0 : panic_P(PSTR("Assert failed in %s:%s at %i"), \
+			__FILE__, __func__, __LINE__)
+
+#endif
+
