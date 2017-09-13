0088fd86d28be44fa8e0dc4ccc8f15a822564791
Michel Megens <dev@bietje.net>
2017-09-13 21:09:01 +0200

    pwm: doc: document the PWM datastructures
    Add Doxygen documentation to the PWM datastructures.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- include/etaos/pwm.h
+++ include/etaos/pwm.h
@@ -16,6 +16,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup pwm
+ * @{
+ */
+
 #ifndef __PWM_H__
 #define __PWM_H__
 
@@ -25,44 +30,86 @@
 #include <etaos/clocksource.h>
 #include <etaos/gpio.h>
 
+/**
+ * @brief Channel duty cycle.
+ */
 typedef union {
-	float fraction;
-	uint16_t time;
+	float fraction; //!< Duty cycle as a fraction of the period.
+	uint16_t time; //!< Duty cycle measured in Hertz.
 } duty_cycle_t;
 
-#define PWM_ENABLED_FLAG             0
-#define PWM_DUTY_CYCLE_FRACTION_FLAG 1
-#define PWM_PIN_HIGH_FLAG            2
+#define PWM_ENABLED_FLAG             0 //!< Enable flag.
+#define PWM_DUTY_CYCLE_FRACTION_FLAG 1 //!< Set if duty cyle is given as a fraction.
+#define PWM_PIN_HIGH_FLAG            2 //!< Set if the PWM channel is currently high.
 
+/**
+ * @brief PWM channel settings.
+ */
 struct pwm_state {
-	unsigned long flags;
-	duty_cycle_t cycle;
+	unsigned long flags; //!< PWM flags.
+	duty_cycle_t cycle; //!< Duty cycle.
 };
 
 struct pwm;
+/**
+ * @brief PWM channel data.
+ */
 struct pwm_channel {
-	struct pwm *base;
+	struct pwm *base; //!< PWM chip to which this channel belongs.
 
-	struct gpio_pin *output;
-	struct pwm_state state;
+	struct gpio_pin *output; //!< Output pin.
+	struct pwm_state state; //!< Current channel settings.
 };
 
+/**
+ * @brief PWM chip driver.
+ */
 struct pwm_driver {
-	void (*start)(struct pwm*,uint32_t);
+	/**
+	 * @brief Start the PWM.
+	 * @param pwm PWM to start.
+	 * @param freq PWM frequency.
+	 */
+	void (*start)(struct pwm *pwm,uint32_t freq);
+
+	/**
+	 * @brief Stop the PWM.
+	 * @param pwm PWM to stop.
+	 */
 	void (*stop)(struct pwm*);
-	void (*update_channel)(struct pwm *, int, struct pwm_state*);
-	int (*update_frequency)(struct pwm*, uint32_t);
+
+	/**
+	 * @brief Update channel features.
+	 * @param pwm PWM chip to which \p chanid belongs.
+	 * @param chanid Channel ID of the channel to edit.
+	 * @param state New channel settings.
+	 */
+	void (*update_channel)(struct pwm *pwm, int chanid, struct pwm_state *state);
+
+	/**
+	 * @brief Update the channel frequency.
+	 * @param pwm PWM chip.
+	 * @param freq Frequency to tune.
+	 * @return An error code.
+	 */
+	int (*update_frequency)(struct pwm *pwm, uint32_t freq);
 };
 
+/**
+ * @brief PWM chip datastructure.
+ *
+ * This structure describes a single device capable of generating
+ * PWM.
+ */
 struct pwm {
-	spinlock_t lock;
-	struct clocksource *src;
-	void *iobase;
-	uint8_t num;
-	uint32_t frequency;
-	struct pwm_driver *driver;
+	spinlock_t lock; //!< Lock.
+	struct clocksource *src; //!< Backend clock.
+	void *iobase; //!< Memory I/O..
+	uint8_t num; //!< Number of channels..
+	uint32_t frequency; //!< Signal frequency in Hertz.
+	struct pwm_driver *driver; //!< PWM driver.
 
-	struct pwm_channel *channels[];
+	struct pwm_channel *channels[]; //!< PWM channels.
 };
 
 CDECL
@@ -74,3 +121,5 @@ extern void pwm_stop(struct pwm *pwm);
 extern int pwm_update_frequency(struct pwm *pwm, uint32_t frequency);
 
 #endif
+
+/** @} */
