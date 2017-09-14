97b74bfb7bd5acc8fd4ac576a5fbc73781c59a63
Michel Megens <dev@bietje.net>
2017-09-14 14:09:32 +0200

    sched: cond: doc: write docs for condition variables
    Add Doxygen documentation to the implementation of condition
    variables.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- kernel/sched/condition.c
+++ kernel/sched/condition.c
@@ -16,6 +16,11 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * @addtogroup condition
+ * @{
+ */
+
 #include <etaos/kernel.h>
 #include <etaos/types.h>
 #include <etaos/thread.h>
@@ -26,6 +31,10 @@
 #include <etaos/assert.h>
 #include <etaos/preempt.h>
 
+/**
+ * @brief Initialise a new condition variable.
+ * @param cond Condition variable to initialise.
+ */
 void condition_init(struct condition *cond)
 {
 	ASSERT(cond != NULL);
@@ -34,6 +43,15 @@ void condition_init(struct condition *cond)
 	mutex_init(&cond->lock);
 }
 
+/**
+ * @brief Wait for \p ms miliseconds on \p condi.
+ * @param condi Condition variable to wait on.
+ * @param ms Number of miliseconds to wait at most.
+ * @return An error code.
+ * @see condition_wait condition_wait_for
+ * @note If \p ms is set to \p EVENT_WAIT_INFINITE, the wait will last until
+ *       a signal is received.
+ */
 static int __condition_wait_for(struct condition *condi, unsigned int ms)
 {
 	int rv;
@@ -47,38 +65,68 @@ static int __condition_wait_for(struct condition *condi, unsigned int ms)
 	return rv;
 }
 
+/**
+ * @brief Wait until another thread signals \p cond.
+ * @param cond The condition to wait on.
+ * @return An error code.
+ */
 int condition_wait(struct condition *cond)
 {
 	ASSERT(cond != NULL);
 	return __condition_wait_for(cond, EVENT_WAIT_INFINITE);
 }
 
+/**
+ * @brief Wait until another thread signals \p cond.
+ * @param condi The condition to wait on.
+ * @param ms Number of miliseconds to wait.
+ * @return An error code.
+ */
 int condition_wait_for(struct condition *condi, unsigned int ms)
 {
 	ASSERT(condi != NULL);
 	return __condition_wait_for(condi, ms);
 }
 
+/**
+ * @brief Signal a condition variable.
+ * @param cond Condition variable to signal.
+ */
 void condition_signal(struct condition *cond)
 {
 	ASSERT(cond != NULL);
 	event_notify(&cond->qp);
 }
 
+/**
+ * @brief Signal all threads waiting on a condition variable.
+ * @param cond Condition variable to broadcast the signal onto.
+ * @return An error code.
+ */
 int condition_bcast(struct condition *cond)
 {
 	ASSERT(cond != NULL);
 	return event_notify_broadcast(&cond->qp);
 }
 
+/**
+ * @brief Lock a condition variable.
+ * @param condi Condition variable to lock.
+ */
 void condition_lock(struct condition *condi)
 {
 	ASSERT(condi != NULL);
 	mutex_lock(&condi->lock);
 }
 
+/**
+ * @brief Unlock a condition variable.
+ * @param condi Condition variable to unlock.
+ */
 void condition_unlock(struct condition *condi)
 {
 	ASSERT(condi != NULL);
 	mutex_unlock(&condi->lock);
 }
+
+/** @} */
