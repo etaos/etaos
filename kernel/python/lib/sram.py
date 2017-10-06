d6a74741e14ba358b0b7ead80316901310d56f85
Michel Megens <dev@bietje.net>
2017-10-07 01:54:13 +0200

    python: sram: add docs to the SRAM package
    Add Doxygen documentation to the SRAM class and its methods in the
    `sram' package.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- kernel/python/lib/sram.py
+++ kernel/python/lib/sram.py
@@ -29,22 +29,39 @@ __name__ = "sram"
 
 ## SRAM class
 class SRAM(Device):
+	## Create a new SRAM object.
+	# @param name Device name.
 	def __init__(self, name):
 		Device.__init__(self, name)
 		self.size = -1
 
+	## Read a string from SRAM.
+	# @param addr Address to read from.
+	# @param length Number of characters to read.
+	# @return String read from SRAM.
 	def read_string(self, addr, length):
 		rv = self.read(length, addr)
 		return rv
 
+	## Write a string to SRAM.
+	# @param addr Address to write to.
+	# @param string String to write.
+	# @return Error code.
 	def write_string(self, addr, string):
 		rv = self.write(string, addr)
 		return rv
 
+	## Read a float from SRAM.
+	# @param addr Address to read a float from.
+	# @return The float read from SRAM.
 	def read_float(self, addr):
 		rv = self.read_numeral(addr, True)
 		return rv
 
+	## Write a float to SRAM.
+	# @param addr Address to write to.
+	# @param num Float to write.
+	# @return An error code.
 	def write_float(self, addr, num):
 		rv = self.write_numeral(num, addr)
 		return rv
