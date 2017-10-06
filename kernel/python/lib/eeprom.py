0f11c7b3ca12c5ebf06c125b7ffda4c7daf06402
Michel Megens <dev@bietje.net>
2017-10-07 00:08:39 +0200

    python: eeprom: add documentation to the EEPROM class
    Add doxygen documentation to all methods within in the EEPROM
    class.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- kernel/python/lib/eeprom.py
+++ kernel/python/lib/eeprom.py
@@ -27,23 +27,41 @@ from device import Device
 
 __name__ = "eeprom"
 
+## EEPROM device extension
 class EEPROM(Device):
+	## Create a new EEPROM object.
+	# @param name Device name.
 	def __init__(self, name):
 		Device.__init__(self, name)
 		self.size = -1
 
+	## Write a string to EEPROM.
+	# @param addr Address to write to.
+	# @param string String to write.
+	# @return Error code.
 	def write_string(self, addr, string):
 		rv = self.write(string, addr)
 		return rv
 
+	## Read a string from EEPROM.
+	# @param addr Address to read from.
+	# @param length Number of characters to read.
+	# @return String read from EEPROM.
 	def read_string(self, addr, length):
 		rv = self.read(length, addr)
 		return rv
 
+	## Read a float from EEPROM.
+	# @param addr Address to read a float from.
+	# @return The float read from EEPROM.
 	def read_float(self, addr):
 		rv = self.read_numeral(addr, True)
 		return rv
 
+	## Write a float to EEPROM.
+	# @param addr Address to write to.
+	# @param num Float to write.
+	# @return An error code.
 	def write_float(self, addr, num):
 		rv = self.write_numeral(num, addr)
 		return rv
